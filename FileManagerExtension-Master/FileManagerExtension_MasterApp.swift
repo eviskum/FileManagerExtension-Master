6cf446cb187466228161cf79d5c4962eb75528ca
Esben Viskum <viskum@viskum.dk>
2021-05-25 10:16:53 +0200

    Initial Commit

---
+++ FileManagerExtension-Master/FileManagerExtension_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileManagerExtension_MasterApp.swift
+//  FileManagerExtension-Master
+//
+//  Created by Esben Viskum on 25/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct FileManagerExtension_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
